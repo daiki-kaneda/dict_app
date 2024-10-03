81d2ca830a715e25e30931e0629bda57d8c6b2c3
daiki-kaneda <daiki0622developing@gmail.com>
2024-10-03 13:38:44 +0900

    hide cupertinoBackButton bug
    - hided animation bug of CupertinoBackButton by using Future.delayed in PopButton

--- lib/widgets/folder_structure_widget/buttons/pop_button.dart
+++ lib/widgets/folder_structure_widget/buttons/pop_button.dart
@@ -2,16 +2,36 @@ import 'package:dict_app/providers/audio_player_provider/audio_player_provider.d
 import 'package:dict_app/providers/dict_view_provider/dict_view_provider.dart';
 import 'package:dict_app/providers/utility%20_provider/utility_provider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-class PopButton extends ConsumerWidget{
+class PopButton extends ConsumerStatefulWidget {
   const PopButton({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _PopButtonState();
+}
+
+class _PopButtonState extends ConsumerState<PopButton> {
+  bool showing = false;
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 1000))
+    .whenComplete((){
+      setState(() {
+        showing=true;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final canPop = ref.watch(canPopProvider);
-    return canPop.value==true ? CupertinoNavigationBarBackButton(
+    return canPop.value==true && showing? 
+    CupertinoNavigationBarBackButton(
               onPressed: () {
                 ref
                     .read(currentTreeIdNotifierProvider.notifier)
@@ -25,6 +45,7 @@ class PopButton extends ConsumerWidget{
                 ref.read(audioPlayerNotifierProvider.notifier).pause();
                 Navigator.of(context).pop();
               },
-            ):const SizedBox();
+            )
+            :const SizedBox();
   }
 }
