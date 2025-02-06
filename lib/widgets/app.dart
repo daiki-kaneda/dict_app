213df95de7cc2ebf3f45d22dbcbec3fe4e87af23
daiki-kaneda <daiki0622developing@gmail.com>
2025-02-06 19:49:20 +0900

    Modify theme of CupertinoApp

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -30,7 +30,6 @@ class App extends ConsumerWidget {
     return _EagerInitialization(
       CupertinoApp(
       theme: const CupertinoThemeData(
-        brightness: Brightness.light, // ライトモードを指定
       ),
       builder: (context, child) {
         if(todoTree.hasValue){
