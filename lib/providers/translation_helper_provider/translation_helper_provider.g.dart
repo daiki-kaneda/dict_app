fb2dfdeb14891e9c034601f2df41d0e125c344be
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-12 11:27:21 +0900

    Add translation_helper,translation_helper_provider

---
+++ lib/providers/translation_helper_provider/translation_helper_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'translation_helper_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$translationHelperHash() => r'86bed4f2790d1d1207708b29eed6334c5961f2ce';
+
+/// See also [translationHelper].
+@ProviderFor(translationHelper)
+final translationHelperProvider =
+    AutoDisposeFutureProvider<EnglishTranslationHelper>.internal(
+  translationHelper,
+  name: r'translationHelperProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$translationHelperHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TranslationHelperRef
+    = AutoDisposeFutureProviderRef<EnglishTranslationHelper>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
