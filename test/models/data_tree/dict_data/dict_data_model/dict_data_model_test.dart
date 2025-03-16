a36fdb13300b95f1d1a9239efe434da7828a0a9d
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-16 22:14:27 +0900

    Modify dict_data_model_test

--- test/models/data_tree/dict_data/dict_data_model/dict_data_model_test.dart
+++ test/models/data_tree/dict_data/dict_data_model/dict_data_model_test.dart
@@ -1,17 +1,25 @@
-import 'package:dict_app/models/data_tree/dict_data/dict_data_model/dictation_data_model.dart';
+import 'package:dict_app/models/data_tree_isar/dictation_data_model/dictation_data_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:characters/characters.dart';
 
 void main() {
-  var dictation = DictationParagraphs(
+  var dictation = DictationSection(
     paragraphs: [
       DictationParagraph(
         sentences: [
           DictationSentence(
             sentence: "Hello world.",
             words: [
-              DictationWord.from(word: "Hello", start: 0, end: 1),
-              DictationWord.from(word: "world.", start: 1, end: 2),
+              DictationWord.from(
+                index: 0,
+                parentIndex: 0,
+                word: "Hello", 
+                start: 0, 
+                end: 1),
+              DictationWord.from(
+                index: 1,
+                parentIndex: 0,
+                word: "world.", start: 1, end: 2),
             ],
             start: 0,
             end: 2,
@@ -19,10 +27,30 @@ void main() {
           DictationSentence(
             sentence: "This is a test.",
             words: [
-              DictationWord.from(word: "This", start: 2, end: 3),
-              DictationWord.from(word: "is", start: 3, end: 4),
-              DictationWord.from(word: "a", start: 4, end: 5),
-              DictationWord.from(word: "test.", start: 5, end: 6),
+              DictationWord.from(
+                index: 0,
+                parentIndex: 1,
+                word: "This", 
+                start: 2, 
+                end: 3),
+              DictationWord.from(
+                index: 1,
+                parentIndex: 1,
+                word: "is", 
+                start: 3, 
+                end: 4),
+              DictationWord.from(
+                index: 2,
+                parentIndex: 1,
+                word: "a", 
+                start: 4, 
+                end: 5),
+              DictationWord.from(
+                index: 3,
+                parentIndex: 1,
+                word: "test.", 
+                start: 5, 
+                end: 6),
             ],
             start: 2,
             end: 6,
@@ -36,7 +64,7 @@ void main() {
   group('tryCharacter test start', () {
     test('correct input test', () {
       void printCharacter(int index) {
-        print(dictation.paragraphs[0].sentences[0].words[1].characters[index]
+        print(dictation.paragraphs![0].sentences![0].words![1].characters![index]
             .toJson());
       }
 
@@ -45,18 +73,20 @@ void main() {
         printCharacter(c.key);
         expect(
             dictation
-                .paragraphs[0].sentences[0].words[1].characters[c.key].isSolved,
+                .paragraphs![0].sentences![0].words![1].characters![c.key].isSolved,
             false);
-        dictation = dictation.tryCharacter(
+        final (newDictation,result) = dictation.tryCharacter(
             input: inputCharacters[c.key],
             paragraphIndex: 0,
             sentenceIndex: 0,
             wordIndex: 1,
             solveAnyway: true);
+        dictation = newDictation;
         expect(
             dictation
-                .paragraphs[0].sentences[0].words[1].characters[c.key].isSolved,
+                .paragraphs![0].sentences![0].words![1].characters![c.key].isSolved,
             true);
+        expect(result.status, c.key==inputCharacters.length-1 ? SolveStatus.wordSolved:SolveStatus.characterSolved);
         printCharacter(c.key);
       }
     });
