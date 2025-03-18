dc2890eff0d3970125e85b9575be3f27eb97fa49
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-18 20:40:18 +0900

    Modify platform_linear_indicator

--- lib/isar_widgets/utils/platform_linear_indicator.dart
+++ lib/isar_widgets/utils/platform_linear_indicator.dart
@@ -5,48 +5,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class PlatformLinearIndicator extends StatelessWidget {
-  final double progress; 
+  const PlatformLinearIndicator(
+      {super.key,
+      required this.progress,
+      this.backgroundColor,
+      this.activeColor});
 
-  const PlatformLinearIndicator({super.key, required this.progress});
+  final double progress;
+  final Color? backgroundColor;
+  final Color? activeColor;
 
   @override
   Widget build(BuildContext context) {
     final isIOS = Platform.isIOS;
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: ColorBar(
+          color: backgroundColor ??
+              (isIOS
+                  ? CupertinoColors.systemGrey5.resolveFrom(context)
+                  : Colors.grey[300]),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: FractionallySizedBox(
+                widthFactor: progress.clamp(0.0, 1.0),
+                child: ColorBar(
+                  color: activeColor ??
+                      (isIOS
+                          ? CupertinoColors.label.resolveFrom(context)
+                          : Colors.grey[600]),
+                )),
+          ),
+        ));
+  }
+}
+
+class ColorBar extends StatelessWidget {
+  const ColorBar({super.key, required this.color, this.child});
+
+  final Color? color;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 4),
       height: 4,
       decoration: BoxDecoration(
-        color:isIOS ? CupertinoColors.systemGrey5.resolveFrom(context).resolveFrom(context):Colors.grey[300],
+        color: color,
         borderRadius: BorderRadius.circular(2),
       ),
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: FractionallySizedBox(
-          widthFactor: progress.clamp(0.0, 1.0),
-          child: Container(
-            height: 4,
-            decoration: BoxDecoration(
-              color:isIOS ? CupertinoColors.label:Colors.grey[600], 
-              borderRadius: BorderRadius.circular(2),
-            ),
-          ),
-        ),
-      ),
+      child: child,
     );
   }
 }
 
-class AnimatedPlatformPageViewLinearIndicator extends AnimatedWidget{
-  const AnimatedPlatformPageViewLinearIndicator(this.controller,{super.key,required this.pageLength}):super(listenable: controller);
+class AnimatedPlatformPageViewLinearIndicator extends AnimatedWidget {
+  const AnimatedPlatformPageViewLinearIndicator(this.controller,
+      {super.key, required this.pageLength})
+      : super(listenable: controller);
 
   final int pageLength;
   final PageController controller;
 
   @override
   Widget build(BuildContext context) {
-    final page = controller.page;
-    if(page==null)return Center(child: PlatformCircularProgressIndicator(),);
+    final page = controller.page ?? 0;
+
     return PlatformLinearIndicator(
-      progress:pageLength!=1 ? controller.page!/(pageLength-1):page);
+        progress: pageLength != 1 ? page / (pageLength - 1) : page);
   }
 }
