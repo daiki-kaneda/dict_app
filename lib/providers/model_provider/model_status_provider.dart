d27547db3cface98e496721100b0314fcd141f3a
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-20 01:04:18 +0900

    Modify ModelStatus

--- lib/providers/model_provider/model_status_provider.dart
+++ lib/providers/model_provider/model_status_provider.dart
@@ -6,7 +6,7 @@ enum ModelStatus {
   String get modelName {
     switch (this) {
       case geminiFlash:
-        return 'gemini-2.0-flash';
+        return 'gemini-2.0-flash-lite';
       case geminiPro:
         return 'gemini-1.5-pro';
     }
