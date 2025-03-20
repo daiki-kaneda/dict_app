d6342a30dc416b3980aa243ae7a022ac13b03cc2
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-20 11:42:12 +0900

    Add intializeDateFormatting to main()

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:dict_app/widgets/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  
+  initializeDateFormatting();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
