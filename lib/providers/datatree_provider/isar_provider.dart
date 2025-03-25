fb5c2ee8e1fdc02419a3c64707d77ef088691a58
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-25 22:42:03 +0900

    Modify isarprovider to add LogEntrySchema

--- lib/providers/datatree_provider/isar_provider.dart
+++ lib/providers/datatree_provider/isar_provider.dart
@@ -1,5 +1,6 @@
 
 import 'package:dict_app/models/data_tree/item.dart';
+import 'package:dict_app/models/log_entry.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -9,6 +10,6 @@ part 'isar_provider.g.dart';
 @riverpod
 Future<Isar> isar(IsarRef ref)async{
   final dir = await getApplicationSupportDirectory();
-  final isar =Isar.open([FolderSchema,FileSchema], directory: dir.path);
+  final isar =Isar.open([FolderSchema,FileSchema,LogEntrySchema], directory: dir.path);
   return isar;
 }
