3337567d0211c24a2abf3b6ab7b657d633e62e8d
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-25 22:08:57 +0900

    Refactor generateDictationDocument

--- lib/widgets/file_details_view/print_view/dictation.dart
+++ lib/widgets/file_details_view/print_view/dictation.dart
@@ -1,11 +1,12 @@
 import 'dart:typed_data';
 
 import 'package:collection/collection.dart';
-import 'package:dict_app/widgets/app.dart';
-import 'package:dict_app/widgets/utils/platform_dialog.dart';
+import 'package:dict_app/models/data_tree/dictation_data_model/dictation_data_model.dart';
 import 'package:dict_app/models/data_tree/item.dart';
 import 'package:dict_app/models/setting.dart';
 import 'package:dict_app/utils/utils.dart';
+import 'package:dict_app/widgets/app.dart';
+import 'package:dict_app/widgets/utils/platform_dialog.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
@@ -15,6 +16,7 @@ class DictationDocumentData {
     required this.file,
     required this.setting,
   });
+
   final File file;
   final Setting setting;
 }
@@ -25,196 +27,185 @@ Future<Uint8List> generateDictationDocument(
   final setting = data.setting;
   final sentences = file.getAllSentences;
   final doc = pw.Document(pageMode: PdfPageMode.outlines);
-  final separateWordsWithParentheses = setting.separateWordsWithParentheses;
 
-  final localizedFont = await getFontForLocale(setting.languageCode);
+  final localizedFont = await _getFont(setting.languageCode);
   if (localizedFont == null) {
     showNotifyDialog(navigatorKey.currentContext!,
         title: '読み込みエラー', description: 'フォントの読み込みでエラーが発生しました。接続環境を確認してください。');
   }
 
-  doc.addPage(pw.MultiPage(
-      theme: pw.ThemeData.withFont(
-        base: localizedFont,
-      ),
+  doc.addPage(
+    pw.MultiPage(
+      theme: pw.ThemeData.withFont(base: localizedFont),
       pageFormat: format.copyWith(marginBottom: 1.5 * PdfPageFormat.cm),
       orientation: pw.PageOrientation.portrait,
       crossAxisAlignment: pw.CrossAxisAlignment.start,
-      header: (pw.Context context) {
-        if (context.pageNumber == 1) {
-          return pw.SizedBox();
-        }
-        return pw.Container(
-            alignment: pw.Alignment.centerRight,
-            margin: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
-            padding: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
-            decoration: const pw.BoxDecoration(
-                border: pw.Border(
-                    bottom: pw.BorderSide(width: 0.5, color: PdfColors.grey))),
-            child: pw.Text(file.title,
-                style: pw.Theme.of(context)
-                    .defaultTextStyle
-                    .copyWith(color: PdfColors.grey)));
-      },
-      footer: (pw.Context context) {
-        return pw.Container(
-            alignment: pw.Alignment.centerRight,
-            margin: const pw.EdgeInsets.only(top: 1.0 * PdfPageFormat.cm),
-            child: pw.Text(
-                setting.showPageNumbers
-                    ? 'Page ${context.pageNumber} of ${context.pagesCount}'
-                    : '',
-                style: pw.Theme.of(context)
-                    .defaultTextStyle
-                    .copyWith(color: PdfColors.grey)));
-      },
-      build: (pw.Context context) => <pw.Widget>[
-            pw.Header(level: 1, text: file.title),
-            for (final t in sentences.indexed) ...[
-              pw.Column(
-                  crossAxisAlignment: pw.CrossAxisAlignment.stretch,
-                  mainAxisSize: pw.MainAxisSize.min,
-                  children: [
-                    pw.Wrap(spacing: 5 * PdfPageFormat.mm, children: [
-                      for (final w in t.$2.words!.map((w) => w.displayText))
-                        pw.Text(
-                            '${separateWordsWithParentheses ? '(' : ''}${w.replaceAll(
-                                    setting.hideOnlyAlphabet
-                                        ? RegExp(r'[a-zA-Z]')
-                                        : RegExp(r'.'),
-                                    setting.showUnderlinedParts ? '_' : ' ')}${separateWordsWithParentheses ? ')' : ''}',
-                            style: pw.TextStyle(
-                                fontSize: 0.8 * PdfPageFormat.cm,
-                                letterSpacing: setting.connectUnderlinedParts
-                                    ? 0
-                                    : 1 * PdfPageFormat.mm))
-                    ]),
-                    if (setting.appendTranslation) ...[
-                      pw.Padding(
-                          padding:
-                              pw.EdgeInsets.only(top: 0.3 * PdfPageFormat.cm)),
-                      pw.Builder(
-                        builder: (context) {
-                          final translatedSentence = file.paragraphs
-                              .translatedSentences(setting.languageCode)
-                              .elementAtOrNull(t.$1);
-                          return pw.Text(translatedSentence ?? '',
-                              style: pw.TextStyle(
-                                fontSize: 0.5 * PdfPageFormat.cm,
-                              ));
-                        },
-                      )
-                    ],
-                    pw.Text(
-                      'Audio: ${formatDuration(Duration(milliseconds: (t.$2.start! * 1000).toInt()))} - ${formatDuration(Duration(milliseconds: (t.$2.end! * 1000).toInt()))}',
-                      textAlign: pw.TextAlign.right,
-                      style: pw.TextStyle(
-                          fontSize: 0.5 * PdfPageFormat.cm,
-                          color: PdfColors.grey),
-                    ),
-                    pw.Padding(
-                        padding: pw.EdgeInsets.only(top: 1 * PdfPageFormat.cm))
-                  ])
-            ]
-          ]));
+      header: (pw.Context context) =>
+          context.pageNumber == 1 ? pw.SizedBox() : _buildHeader(context, file),
+      footer: (pw.Context context) => _buildFooter(context, setting),
+      build: (pw.Context context) => [
+        pw.Header(level: 1, text: file.title),
+        ..._buildDictationContent(context, sentences, setting, file),
+      ],
+    ),
+  );
 
   if (setting.appendAnswer) {
-    doc.addPage(pw.MultiPage(
-        theme: pw.ThemeData.withFont(
-          base: localizedFont,
-        ),
+    doc.addPage(
+      pw.MultiPage(
+        theme: pw.ThemeData.withFont(base: localizedFont),
         pageFormat: format.copyWith(marginBottom: 1.5 * PdfPageFormat.cm),
         orientation: pw.PageOrientation.portrait,
         crossAxisAlignment: pw.CrossAxisAlignment.start,
-        header: (pw.Context context) {
-          return pw.Container(
-              alignment: pw.Alignment.centerRight,
-              margin: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
-              padding: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
-              decoration: const pw.BoxDecoration(
-                  border: pw.Border(
-                      bottom:
-                          pw.BorderSide(width: 0.5, color: PdfColors.grey))),
-              child: pw.Text(file.title,
-                  style: pw.Theme.of(context)
-                      .defaultTextStyle
-                      .copyWith(color: PdfColors.grey)));
-        },
-        footer: (pw.Context context) {
-          return pw.Container(
-              alignment: pw.Alignment.centerRight,
-              margin: const pw.EdgeInsets.only(top: 1.0 * PdfPageFormat.cm),
-              child: pw.Text(
-                  'Page ${context.pageNumber} of ${context.pagesCount}',
-                  style: pw.Theme.of(context)
-                      .defaultTextStyle
-                      .copyWith(color: PdfColors.grey)));
-        },
-        build: (pw.Context context) => <pw.Widget>[
-              pw.Header(level: 2, text: 'Answers'),
-              for (final t in sentences.indexed) ...[
-                pw.Column(
-                    crossAxisAlignment: pw.CrossAxisAlignment.stretch,
-                    mainAxisSize: pw.MainAxisSize.min,
-                    children: [
-                      pw.Wrap(spacing: 2 * PdfPageFormat.mm, children: [
-                        for (final w in t.$2.words!.map((w) => w.displayText))
-                          pw.Text(w,
-                              style: pw.TextStyle(
-                                  fontSize: 0.5 * PdfPageFormat.cm))
-                      ]),
-                      if (setting.appendTranslation) ...[
-                        pw.Padding(
-                            padding: pw.EdgeInsets.only(
-                                top: 0.3 * PdfPageFormat.cm)),
-                        pw.Builder(
-                          builder: (context) {
-                            final translatedSentence = file.paragraphs
-                                .translatedSentences(setting.languageCode)
-                                .elementAtOrNull(t.$1);
-                            return pw.Text(translatedSentence ?? '',
-                                style: pw.TextStyle(
-                                  fontSize: 0.5 * PdfPageFormat.cm,
-                                ));
-                          },
-                        )
-                      ],
-                      pw.Text(
-                        'Audio: ${formatDuration(Duration(milliseconds: (t.$2.start! * 1000).toInt()))} - ${formatDuration(Duration(milliseconds: (t.$2.end! * 1000).toInt()))}',
-                        textAlign: pw.TextAlign.right,
-                        style: pw.TextStyle(
-                            fontSize: 0.5 * PdfPageFormat.cm,
-                            color: PdfColors.grey),
-                      ),
-                      pw.Padding(
-                          padding:
-                              pw.EdgeInsets.only(top: 1 * PdfPageFormat.cm))
-                    ])
-              ]
-            ]));
+        header: (pw.Context context) => _buildHeader(context, file),
+        footer: (pw.Context context) => _buildFooter(context, setting,
+            showPageNumbers: false),
+        build: (pw.Context context) => [
+          pw.Header(level: 2, text: 'Answers'),
+          ..._buildAnswerContent(context, sentences, setting, file),
+        ],
+      ),
+    );
   }
 
   return await doc.save();
 }
 
-Future<pw.Font?> getFontForLocale(String languageCode) async {
-  if (languageCode == 'ja') {
-    return PdfGoogleFonts.notoSansJPRegular();
-  } else if (languageCode == 'ko') {
-    return PdfGoogleFonts.notoSansKRRegular();
-  } else if (languageCode == 'zh') {
-    return PdfGoogleFonts.notoSansSCRegular();
-  } else if (languageCode == 'hi') {
-    return PdfGoogleFonts.notoSansDevanagariRegular();
-  } else if (languageCode == 'bn') {
-    return PdfGoogleFonts.notoSansBengaliRegular();
-  } else if (languageCode == 'ta') {
-    return PdfGoogleFonts.notoSansTamilRegular();
-  } else if (languageCode == 'ar' ||
-      languageCode == 'ur' ||
-      languageCode == 'fa') {
-    return PdfGoogleFonts.notoSansArabicRegular();
-  } else {
-    return PdfGoogleFonts.notoSansRegular();
+pw.Widget _buildHeader(pw.Context context, File file) {
+  return pw.Container(
+    alignment: pw.Alignment.centerRight,
+    margin: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+    padding: const pw.EdgeInsets.only(bottom: 3.0 * PdfPageFormat.mm),
+    decoration: const pw.BoxDecoration(
+      border:
+          pw.Border(bottom: pw.BorderSide(width: 0.5, color: PdfColors.grey)),
+    ),
+    child: pw.Text(
+      file.title,
+      style:
+          pw.Theme.of(context).defaultTextStyle.copyWith(color: PdfColors.grey),
+    ),
+  );
+}
+
+pw.Widget _buildFooter(pw.Context context, Setting setting,
+    {bool showPageNumbers = true}) {
+  return pw.Container(
+    alignment: pw.Alignment.centerRight,
+    margin: const pw.EdgeInsets.only(top: 1.0 * PdfPageFormat.cm),
+    child: pw.Text(
+      showPageNumbers && setting.showPageNumbers
+          ? 'Page ${context.pageNumber} of ${context.pagesCount}'
+          : '',
+      style:
+          pw.Theme.of(context).defaultTextStyle.copyWith(color: PdfColors.grey),
+    ),
+  );
+}
+
+List<pw.Widget> _buildDictationContent(pw.Context context,
+    List<DictationSentence> sentences, Setting setting, File file) {
+  return sentences.indexed
+      .map((t) =>
+          _buildSentenceSection(context, t, setting, file, isAnswer: false))
+      .toList();
+}
+
+List<pw.Widget> _buildAnswerContent(pw.Context context,
+    List<DictationSentence> sentences, Setting setting, File file) {
+  return sentences.indexed
+      .map((t) =>
+          _buildSentenceSection(context, t, setting, file, isAnswer: true))
+      .toList();
+}
+
+pw.Widget _buildSentenceSection(
+    pw.Context context, (int, DictationSentence) t, Setting setting, File file,
+    {required bool isAnswer}) {
+  final textStyle = pw.TextStyle(fontSize: 0.8 * PdfPageFormat.cm);
+  final smallTextStyle = pw.TextStyle(fontSize: 0.5 * PdfPageFormat.cm);
+  final greyTextStyle =
+      pw.TextStyle(fontSize: 0.5 * PdfPageFormat.cm, color: PdfColors.grey);
+
+  String _maskWord(String word, Setting setting) {
+    if (isAnswer) return word;
+
+    return '${setting.separateWordsWithParentheses ? '(' : ''}${word.replaceAll(setting.hideOnlyAlphabet ? RegExp(r'[a-zA-Z]') : RegExp(r'.'), setting.showUnderlinedParts ? '_' : ' ')}${setting.separateWordsWithParentheses ? ')' : ''}';
+  }
+
+  return pw.Column(
+    crossAxisAlignment: pw.CrossAxisAlignment.stretch,
+    mainAxisSize: pw.MainAxisSize.min,
+    children: [
+      pw.Wrap(
+        spacing: isAnswer ? 2 * PdfPageFormat.mm : 5 * PdfPageFormat.mm,
+        children: t.$2.words!
+            .map((w) => pw.Text(
+                isAnswer ? w.displayText : _maskWord(w.displayText, setting),
+                style: isAnswer
+                    ? smallTextStyle
+                    : textStyle.copyWith(
+                        letterSpacing: setting.connectUnderlinedParts
+                            ? 0
+                            : 1 * PdfPageFormat.mm,
+                      )))
+            .toList(),
+      ),
+      if (setting.appendTranslation)
+        pw.Padding(
+          padding: pw.EdgeInsets.only(top: 0.3 * PdfPageFormat.cm),
+          child: pw.Builder(
+            builder: (context) {
+              final translatedSentence = file.paragraphs
+                  .translatedSentences(setting.languageCode)
+                  .elementAtOrNull(t.$1);
+              return pw.Text(translatedSentence ?? '', style: smallTextStyle);
+            },
+          ),
+        ),
+      pw.Text(
+        'Audio: ${formatDuration(Duration(milliseconds: (t.$2.start! * 1000).toInt()))} - ${formatDuration(Duration(milliseconds: (t.$2.end! * 1000).toInt()))}',
+        textAlign: pw.TextAlign.right,
+        style: greyTextStyle,
+      ),
+      pw.Padding(padding: pw.EdgeInsets.only(top: 1 * PdfPageFormat.cm)),
+    ],
+  );
+}
+
+Future<pw.Font?> _getFont(String languageCode) async {
+  pw.Font? font;
+  try {
+    switch (languageCode) {
+      case 'ja':
+        font = await PdfGoogleFonts.notoSansJPRegular();
+        break;
+      case 'ko':
+        font = await PdfGoogleFonts.notoSansKRRegular();
+        break;
+      case 'zh':
+        font = await PdfGoogleFonts.notoSansSCRegular();
+        break;
+      case 'hi':
+        font = await PdfGoogleFonts.notoSansDevanagariRegular();
+        break;
+      case 'bn':
+        font = await PdfGoogleFonts.notoSansBengaliRegular();
+        break;
+      case 'ta':
+        font = await PdfGoogleFonts.notoSansTamilRegular();
+        break;
+      case 'ar':
+      case 'ur':
+      case 'fa':
+        font = await PdfGoogleFonts.notoSansArabicRegular();
+        break;
+      default:
+        font = await PdfGoogleFonts.notoSansRegular();
+    }
+    return font;
+  } catch (e) {
+    print(e);
+    return null;
   }
 }
