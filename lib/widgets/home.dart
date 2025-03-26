ba9259dbc8965ba7777d20d18d0db6cf0d9dd8cf
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-27 00:25:09 +0900

    Modify Appbar in Home

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -5,23 +5,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return PlatformScaffold(
-      appBar: PlatformAppBar(
-        title: Text('フォルダ'),
-        leading: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-          ShowSettingViewButton(isHome: true,),
-          ShowLogsViewButton()
-        ],),
-      ),
-      body: SubItemsList(parentId: null)
-      );
+        appBar: PlatformAppBar(
+          title: Text('フォルダ'),
+          leading: ShowSettingViewButton(
+              isHome: true,
+            ),
+          trailingActions: [
+            ShowLogsViewButton()
+          ],
+        ),
+        body: SubItemsList(parentId: null));
   }
 }
