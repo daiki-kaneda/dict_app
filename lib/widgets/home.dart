71e54c9dc60d55d168ad24cbd68e01dcf2e96945
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-29 23:54:01 +0900

    Modify Home

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -12,7 +12,7 @@ class Home extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return PlatformScaffold(
         appBar: PlatformAppBar(
-          title: Text('フォルダ'),
+          title: Text('Folder'),
           leading: ShowSettingViewButton(
               isHome: true,
             ),
