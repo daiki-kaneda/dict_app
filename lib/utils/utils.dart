81b6ea4458633170195a0a80ed47fe462cd6b64b
daiki-kaneda <daiki0622developing@gmail.com>
2025-03-30 11:55:06 +0900

    Modify iconDescriptionTextSpan

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -180,7 +180,7 @@ TextSpan iconDescriptionTextSpan(BuildContext context,
             ? CupertinoTheme.of(context).primaryColor
             : Theme.of(context).colorScheme.primary,
         size: fontSize),
-    TextSpan(text: '→ $description', style: TextStyle(fontSize: fontSize))
+    TextSpan(text: ': $description', style: TextStyle(fontSize: fontSize))
   ]);
 }
 
